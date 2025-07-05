75843b1343f293ba34c2f7a34f74ecdcd0d8b3f7
Farman029 <159273036+Farman029@users.noreply.github.com>
2025-07-05 11:53:40 +0530

    Update Signup.jsx

--- Backend/Frontend/src/components/Signup.jsx
+++ Backend/Frontend/src/components/Signup.jsx
@@ -30,7 +30,7 @@ function Signup() {
     };
     // console.log(userInfo);
     await axios
-      .post("/api/user/signup", userInfo)
+      .post("https://yt-chat-bu9p.onrender.com/api/user/signup", userInfo)
       .then((response) => {
         if (response.data) {
           toast.success("Signup successful");
